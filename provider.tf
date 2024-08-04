a60dac4f9d2167b4e50893d6faada62986eebce6
Jala <xyz@gmail.com>
2024-08-04 15:27:03 +0530

    updated provider.tf

--- provider.tf
+++ provider.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.60.0"
-    }
-  }
-}
-
 provider "aws" {
   region = var.aws_region
   profile = "default"

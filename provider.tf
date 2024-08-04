c9e06ecf5ca950506ee223df349f5f8f51402763
Jala <xyz@gmail.com>
2024-08-04 15:44:28 +0530

    updated provider.tf

--- provider.tf
+++ provider.tf
@@ -1,4 +1,6 @@
 provider "aws" {
   region = var.aws_region
+  shared_config_files      = ["/home/ubuntu/.aws/conf"]
+  shared_credentials_files = ["/home/ubuntu/.aws/creds"]
   profile = "default"
 }

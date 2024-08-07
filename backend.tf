4ccb5a08452a0523a287922e362f926feef5df23
Ubuntu <ubuntu@ip-172-31-9-29.ap-south-1.compute.internal>
2024-08-07 05:37:27 +0000

    helm charts

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "terraform-backend0408"
-    key            = "terraform/state.tfstate"
-    region         = "ap-southeast-1"
-    encrypt        = true
-    dynamodb_table = "terraform-dynamo-db-lock"  # Optional, for state locking
-  }
-}
